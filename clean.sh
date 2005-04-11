09abe6b9f13fd8558ec90fa0f53de8641a9c6789
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-04-11 03:15:53 +0000

    Removed references to deleted files.

--- clean.sh
+++ clean.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 make clean
-rm -f config.status config.cache config.log configure Makefile.in Makefile aclocal.m4 confdefs.h gtk-splitter.spec build_rpm.sh build_tgz.sh build_deb.sh
-rm -f *.rpm
-rm -f *.tgz
-rm -f *.deb
+rm -f config.status config.cache config.log configure Makefile.in Makefile aclocal.m4 confdefs.h
 rm -f src/Makefile.in
 rm -f src/Makefile
 rm -fr src/.deps
