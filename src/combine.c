28c93cb4ef3447739b0aa742ea88a9edcb5cce3b
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-04-18 04:40:31 +0000

    Determine the combine data in a separate function. Use fread() & fwrite() for faster I/O. Massive code cleanup.

--- src/combine.c
+++ src/combine.c
@@ -1,5 +1,5 @@
 /*
- * $Id: combine.c,v 1.25 2005/04/15 02:24:09 techgunter Exp $
+ * $Id: combine.c,v 1.26 2005/04/16 22:57:11 techgunter Exp $
  *
  * Copyright 2001 Gunter Wambaugh
  *
@@ -26,304 +26,264 @@
 #include <stdio.h>
 #include <string.h>
 #include <gtk/gtk.h>
+#include <glib.h>
+#include <glib/gstdio.h>
 #include <sys/stat.h>
 #include "session.h"
 #include "window.h"
 #include "progress.h"
 #include "interface.h"
+#include "combine.h"
+
+gboolean
+gtk_splitter_combine_files_test (GtkSplitterSessionData * data)
+{
+  combine_info info;
+  GString *source;
+  gulong size;
+  int i;
+  
+  get_info(&info, data);
+  
+  g_print ("blocksize: %d\n", info.block_size);
+  g_print ("%i->%s (%li)\n", info.number_of_source_files, 
+           info.destination_file->str,
+           info.destination_file_size);
+  
+  for (i = 0; i != info.number_of_source_files; i++)
+    {
+      source = g_array_index (info.source_files, GString *, i);
+      size = g_array_index (info.source_file_sizes, gulong, i);
+       
+      g_print ("%d) %s (%ld)\n", i + 1, source->str, size);
+    }
+  
+  destroy_info (&info);
+  
+  return FALSE;
+}
 
 gboolean
 gtk_splitter_combine_files (GtkSplitterSessionData * data)
 {
-  /* Progress window stuff. */
   ProgressWindow *progress_window = NULL;
-  gboolean do_progress, done;
-
-  /* File streams. */
-  FILE *in, *out;
-
-  /* Various counters. */
-  guint file_count, temp, files_to_combine;
-  gulong combined_file_size, byte_count, bytes_read;
-
-  /* The name of the first file part (including the path). */
-  gchar infile[PATH_MAX];
-
-  /* The name of the combined file. */
-  gchar outfile[PATH_MAX];
-
-  /* The extension for file parts. */
-  gchar ext[] = { "001" };
-
-  struct stat file_info;
-
-#ifdef HAVE_LIBMHASH
-  /* For mhash. */
-  unsigned char hash[16];
-  int hashchar;
-  MHASH thread;
-  gchar md5_file[PATH_MAX];
-  gboolean verified;
-  FILE *md5;
-  unsigned char hashtemp[3] = { '\0' };
-  unsigned char new_hash[33] = { '\0' };
-  unsigned char given_hash[33] = { '\0' };
+  combine_info info;
+  GString *source;
+  gchar *buffer;
+  gulong size;
+  gulong bytes;
+  gulong total_bytes;
+  gulong bytes_fread;
+  gulong bytes_fwrite;
+  
   int i;
-#endif
-
-  /* Setup the infile string. */
-  strcpy (infile, data->file_name_and_path);
-
-  /* Setup the outfile string. */
-  strcpy (outfile, data->output_directory);
-  strcat (outfile, data->file_name_only);
-
-  /* The outfile to be created is the file_name_only minus the '.00x' extension.
-     (i.e. the end of outfile might be:  [.] [0] [0] [1].) */
-  outfile[strlen (outfile) - 4] = '\0';
-
-  /* Do some pre-combine calcualations. */
-  files_to_combine = 0;
-  combined_file_size = 0;
-  done = FALSE;
-  do
-    {
-      in = fopen (infile, "rb");
-      /* If the file cannot be opened, assume it does not exist.
-         Iterate through the extensions.  (.001, .002, .003, ...) */
-      if (in == NULL)
-        done = TRUE;
-      else
-        {
-          files_to_combine++;
-
-          stat (infile, &file_info);
-          combined_file_size += file_info.st_size;
-
-          /* Increment the extension. */
-          if (ext[2] != '9')
-            ext[2]++;
-          else
-            {
-              ext[2] = '0';
-              if (ext[1] != '9')
-                ext[1]++;
-              else
-                {
-                  ext[1] = '0';
-                  ext[0]++;
-                }
-            }
-
-          /* Move on to the next file. */
-          fclose (in);
-          infile[strlen (infile) - 3] = '\0';
-          strcat (infile, ext);
-        }
-
-    }
-  while (!done);
-  /* End of pre-combine calculations.
-     We now know how many files there are to be combined, and what the size
-     of the combined file should be. */
-
+  FILE *in, *out;
+  
+  get_info(&info, data);
+  
   /* Make sure the files don't exceed our limit. */
-  if (files_to_combine > 999)
+  if (info.number_of_source_files > 999)
     {
       display_error ("combine.c:  Exceeded maximum number of files (999).");
       return FALSE;
     }
 
-  /* Reset the extension counter and the infile. */
-  strcpy (ext, "001");
-  infile[strlen (infile) - 3] = '\0';
-  strcat (infile, ext);
-
   /* Open a file to combine the other files to. */
-  out = fopen (outfile, "wb+");
+  out = fopen (info.destination_file->str, "wb+");
   if (out == NULL)
     {
       display_error ("combine.c:  Could not create an output file.");
       return FALSE;
     }
 
-  /* Decide whether or not a progress window would be beneficiary. */
-  if (combined_file_size <= UPDATE_INTERVAL)
-    do_progress = FALSE;
-  else
+  progress_window = progress_window_new ();
+  if (progress_window == NULL)
     {
-      do_progress = TRUE;
-      progress_window = progress_window_new ();
-      if (progress_window == NULL)
-        do_progress = FALSE;
-      else
-        {
-          gtk_window_set_title (GTK_WINDOW (progress_window->base_window),
-                                "Combine Progress");
-          gtk_widget_show_all (progress_window->base_window);
-          while (g_main_context_iteration (NULL, FALSE));
-        }
+      display_error ("combine.c:  Could not create a progress window.");
+      return FALSE;
     }
 
-  /* Now DO the combine. */
-  bytes_read = 0;
-  for (file_count = 0; file_count != files_to_combine; file_count++)
+  gtk_window_set_title (GTK_WINDOW (progress_window->base_window),
+                        "Combine Progress");
+  gtk_widget_show_all (progress_window->base_window);
+  while (g_main_context_iteration (NULL, FALSE));
+
+  buffer = g_malloc (info.block_size * sizeof (gchar));
+  
+  total_bytes = 0;
+  for (i = 0; i != info.number_of_source_files; i++)
     {
-      if (do_progress)
-        gtk_label_set_text (GTK_LABEL (progress_window->message), infile);
-      in = fopen (infile, "rb");
+      source = g_array_index (info.source_files, GString *, i);
+      size = g_array_index (info.source_file_sizes, gulong, i);
+      
+      gtk_label_set_text (GTK_LABEL (progress_window->message), 
+                          g_path_get_basename (source->str)); // needs free ?
+      
+      in = fopen (source->str, "rb");
       if (in == NULL)
         {
           display_error
             ("combine.c:  Could not open one of the files to be combined.");
-          if (do_progress)
-            progress_window_destroy (progress_window);
+          progress_window_destroy (progress_window);
           fclose (out);
           return FALSE;
         }
-      stat (infile, &file_info);
-      for (byte_count = 0; byte_count != file_info.st_size; byte_count++)
+      
+      bytes = 0;
+      bytes_fread = 0;
+      bytes_fwrite = 0;
+      while (!feof (in))
         {
-          bytes_read++;
-          temp = fgetc (in);
-          fputc (temp, out);
-          if ((do_progress) && ((byte_count % UPDATE_INTERVAL) == 0))
-            {
-              gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR
-                                             (progress_window->
-                                              current_progress),
-                                             ((gfloat) byte_count) /
-                                             ((gfloat) file_info.st_size));
-              gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR
-                                             (progress_window->
-                                              total_progress),
-                                             ((gfloat) bytes_read) /
-                                             ((gfloat) combined_file_size));
-              while (g_main_context_iteration (NULL, FALSE));
-            }
+          bytes_fread = fread (buffer, sizeof (gchar), info.block_size, in);
+          bytes_fwrite = fwrite (buffer, sizeof (gchar), bytes_fread, out);
+          bytes += bytes_fread;
+          total_bytes += bytes_fread;
+          
+          gtk_progress_bar_set_fraction (
+              GTK_PROGRESS_BAR (progress_window->current_progress),
+              ((gfloat) bytes) / ((gfloat) size));
+              
+          gtk_progress_bar_set_fraction (
+              GTK_PROGRESS_BAR (progress_window->total_progress),
+              ((gfloat) total_bytes) / ((gfloat) info.destination_file_size));
+              
+          while (g_main_context_iteration (NULL, FALSE));
         }
 
       /* Insure that all data is written to disk before we close. */
       fflush (out);
-
-      /* Increment the extension. */
-      if (ext[2] != '9')
-        ext[2]++;
-      else
-        {
-          ext[2] = '0';
-          if (ext[1] != '9')
-            ext[1]++;
-          else
-            {
-              ext[1] = '0';
-              ext[0]++;
-            }
-        }
 
       /* Move on to the next file. */
       if (fclose (in) == EOF)
         {
           display_error
             ("combine.c:  Could not open one of the files to be combined.");
-          if (do_progress)
-            progress_window_destroy (progress_window);
+          progress_window_destroy (progress_window);
           fclose (out);
           return FALSE;
         }
-
-      infile[strlen (infile) - 3] = '\0';
-      strcat (infile, ext);
     }
   /* End of the combine process. */
 
-#ifdef HAVE_LIBMHASH
-  /* Verify if desired. */
-  if (data->verify)
-    {
-      verified = TRUE;
-      hashchar = '\0';
-      if (do_progress)
-        gtk_label_set_text (GTK_LABEL (progress_window->message),
-                            "Verifying file...");
-
-      thread = mhash_init (MHASH_MD5);
-
-      if (thread == MHASH_FAILED)
-        display_error ("combine.c:  Error generating md5 sum.");
-      else
-        {
-          bytes_read = 0;
-          fseek (out, 0, SEEK_SET);
-          while (fread (&temp, 1, 1, out) == 1)
-            {
-              mhash (thread, &temp, 1);
-              bytes_read++;
-              if ((do_progress) && ((bytes_read % UPDATE_INTERVAL) == 0))
-                {
-                  gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR
-                                                 (progress_window->
-                                                  current_progress),
-                                                 ((gfloat) bytes_read) /
-                                                 ((gfloat)
-                                                  combined_file_size));
-                  gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR
-                                                 (progress_window->
-                                                  total_progress),
-                                                 ((gfloat) bytes_read) /
-                                                 ((gfloat)
-                                                  combined_file_size));
-                  while (g_main_iteration (FALSE));
-                }
-            }
-
-          mhash_deinit (thread, hash);
-          for (i = 0; i < mhash_get_block_size (MHASH_MD5); i++)
-            {
-              sprintf (hashtemp, "%.2x", hash[i]);
-              strcat (new_hash, hashtemp);
-            }
-
-          infile[strlen (infile) - 4] = '\0';
-          sprintf (md5_file, "%s.md5", infile);
-          md5 = fopen (md5_file, "r");
-
-          if (md5 != NULL)
-            {
-              while (hashchar != EOF)
-                {
-                  hashchar = fgetc (md5);
-                  if (hashchar != EOF)
-                    strcat (given_hash, (char *) &hashchar);
-                }
-              fclose (md5);
-
-              if (strcmp (new_hash, given_hash) != 0)
-                verified = FALSE;
-              display_verification (verified);
-            }
-          else
-            {
-              display_error ("split.c:  Error reading md5 sum from file.");
-            }
-        }
-    }
-#endif
+  g_free (buffer);
   
     /* Close our newly combined file. */
-    if (fclose (out) == EOF)
+  if (fclose (out) == EOF)
     {
       display_error ("combine.c:  Could not close the combined file.");
-      if (do_progress)
-        progress_window_destroy (progress_window);
+      progress_window_destroy (progress_window);
       return TRUE;
     }
 
-  if (do_progress)
-    {
-      gtk_widget_hide_all (progress_window->base_window);
-      progress_window_destroy (progress_window);
-    }
-
+  gtk_widget_hide_all (progress_window->base_window);
+  progress_window_destroy (progress_window);
+  destroy_info (&info);
+  
   return TRUE;
+}
+
+gboolean
+get_info (combine_info * info, GtkSplitterSessionData * data)
+{
+  struct stat file_info;
+  GString *source;
+  GString *current_file;
+  GString *extension;
+  FILE *test;
+  
+  /* Set the destination file. */
+  info->destination_file = g_string_new (data->output_directory);
+  info->destination_file = g_string_append_c (info->destination_file, '/');
+  info->destination_file = g_string_append (info->destination_file,
+                                            data->file_name_only);
+  info->destination_file = g_string_truncate (info->destination_file,
+                                              info->destination_file->len - 4);
+  
+  /* Create an array for the file sizes. */
+  info->source_file_sizes = g_array_new (FALSE, FALSE, sizeof (gulong));
+  
+  /* Create an array for the source filenames. */
+  info->source_files = g_array_new (FALSE, FALSE, sizeof (GString *));
+  
+  /* Setup the input file. */
+  extension = g_string_new ("001");
+  source = g_string_new (data->file_name_and_path);
+  
+  /* Should already end in 001. */
+  source = g_string_truncate (source, source->len - 3);
+  source = g_string_append (source, extension->str);
+  
+  info->number_of_source_files = 0;
+  info->destination_file_size = 0;
+  
+  info->block_size = -1;
+  
+  while ((test = fopen (source->str, "rb")) != NULL)
+    {
+      fclose (test);
+      
+      info->number_of_source_files++;
+
+      /* Add to the array of source files. */
+      current_file = g_string_new (source->str);
+      g_array_append_val (info->source_files, current_file);
+      
+      if (g_stat (source->str, &file_info) == -1)
+        {
+          display_error ("combine.c:  Could not stat file.");
+          return FALSE;
+        }
+      
+      info->destination_file_size += file_info.st_size;
+      
+      if (info->block_size == -1)
+        {
+          // Always 0?
+          //info->block_size = file_info.st_blksize;
+          info->block_size = 131072;
+        }
+      
+      /* Add to the array of source file sizes. */
+      g_array_append_val (info->source_file_sizes, file_info.st_size);
+      
+      /* Increment the extension. */
+      if (extension->str[2] != '9')
+        extension->str[2]++;
+      else
+        {
+          extension->str[2] = '0';
+          if (extension->str[1] != '9')
+            extension->str[1]++;
+          else
+            {
+              extension->str[1] = '0';
+              extension->str[0]++;
+            }
+        }
+      
+      /* Next file to open. */
+      source = g_string_truncate (source, source->len - 3);
+      source = g_string_append (source, extension->str);
+    }
+    
+  return TRUE;
+}
+
+void
+destroy_info (combine_info * info)
+{
+  GString *source;
+  int i;
+  
+  /* Free all the GStrings. */
+  for (i = 0; i != info->number_of_source_files; i++)
+    {
+      source = g_array_index (info->source_files, GString *, i);
+      
+      g_string_free (source, TRUE);
+    }
+    
+  /* Free the GArrays. */
+  g_array_free (info->source_file_sizes, TRUE);
+  g_array_free (info->source_files, TRUE);
 }
